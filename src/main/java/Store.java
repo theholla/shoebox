b1fb0bda9f0e8d125254947cb184e9547deabbd6
theholla <holla@bikefarm.org>
2015-09-04 17:31:50 -0700

    Store class instantiates and initial tests pass

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,3 +1,48 @@
+import java.util.List;
+import org.sql2o.*;
+import java.util.ArrayList;
+
 public class Store {
+  private int id;
+  private String company;
+  private String phone;
+  private String address;
+
+  public int getId() {
+    return id;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public Store(String company, String phone, String address) {
+    this.company = company;
+    this.phone = phone;
+    this.address = address;
+  }
+
+  public static List<Store> all() {
+    String sql = "SELECT * FROM stores ORDER BY company ASC";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Store.class);
+    }
+  }
+
+  public static Store firstDBEntry() {
+    return all().get(0);
+  }
+
+  public static int listSize() {
+    return all().size();
+  }
 
 }
--- src/test/java/StoreTest.java
+++ src/test/java/StoreTest.java
@@ -7,4 +7,20 @@ public class StoreTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  @Test public void store_instantiatesCorrectly_true() {
+    Store testStore = new Store("Imeldons", "408-333-2222", "72 NW 45th Ave");
+    assertEquals(true, testStore instanceof Store);
+  }
+
+  @Test
+  public void getCompany_returnsStoreName() {
+    Store testStore = new Store("Imeldons", "408-333-2222", "72 NW 45th Ave");
+    assertEquals("Imeldons", testStore.getCompany());
+  }
+
+  @Test
+  public void all_ListEmptyAtFirst_0() {
+    assertEquals(Store.listSize(), 0);
+  }
+
 }
