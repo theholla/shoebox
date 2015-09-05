fb56a09692bb7843d9cc54f04b083ac46fa23f3f
Diana Holland <holla@bikefarm.org>
2015-09-05 16:54:24 -0700

    New store form on index page, two columns

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,24 +17,37 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    /* Index --> List of Brands */
+    /* Index --> list of Brands */
     get("/brands", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      List<Brand> brands = Brand.all();
-      model.put("brands", brands);
-      // model.put("allStores", Store.all());
+      model.put("brands", Brand.all());
+      model.put("stores", Store.all());
       model.put("template", "templates/brands.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    /* Index --> List of Stores */
+    /* Index --> list of Stores */
     get("/stores", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      List<Store> stores = Store.all();
-      model.put("stores", stores);
-      // model.put("allBrands", Brand.all());
+      model.put("brands", Brand.all());
+      model.put("stores", Store.all());
       model.put("template", "/templates/stores.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+  /* List of Stores -> POST a new store*/
+    post("/stores", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String company = request.queryParams("company");
+      String phone = request.queryParams("phone");
+      String address = request.queryParams("address");
+      Store newStore = new Store(company, phone, address);
+      newStore.save();
+      response.redirect("/stores");
+      return null;
+    });
+
+
+
   }
 }
