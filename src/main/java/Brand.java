aa70771008ee4dd406d4bdf94c3d89499ff35da1
theholla <holla@bikefarm.org>
2015-09-04 17:18:38 -0700

    Comprehensive unit tests for Brand class pass

--- src/main/java/Brand.java
+++ src/main/java/Brand.java
@@ -30,4 +30,63 @@ public class Brand {
     this.stylishness = stylishness;
   }
 
+  public static List<Brand> all() {
+    String sql = "SELEcT * FROM brands ORDER BY name ASC";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Brand.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherBrand) {
+    if (!(otherBrand instanceof Brand)) {
+      return false;
+    } else {
+      Brand newBrand = (Brand) otherBrand;
+      return this.getName().equals(newBrand.getName()) &&
+             this.getPriciness() == newBrand.getPriciness() &&
+             this.getStylishness() == newBrand.getStylishness() &&
+             this.getId() == newBrand.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO brands (name, priciness, stylishness) VALUES (:name, :priciness, :stylishness)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", name)
+      .addParameter("priciness", priciness)
+      .addParameter("stylishness", stylishness)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static Brand find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM brands where id=:id";
+      Brand brand = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Brand.class);
+      return brand;
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteQuery = "DELETE FROM brands WHERE id = :id;";
+      con.createQuery(deleteQuery)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
+
+  public static Brand firstDBEntry() {
+    return all().get(0);
+  }
+
+  public static int listSize() {
+    return all().size();
+  }
+
 }
--- src/test/java/BrandTest.java
+++ src/test/java/BrandTest.java
@@ -9,8 +9,65 @@ public class BrandTest {
 
   @Test
   public void brand_instantiatesCorrectly_true() {
-    Brand testBrand = new Brand("ShoeChic", 5, 3);
+    Brand testBrand = new Brand("Merrep", 3, 4);
     assertEquals(true, testBrand instanceof Brand);
+  }
+
+  @Test
+  public void getName_returnsBrandName() {
+    Brand testBrand = new Brand("LA Geared", 5, 3);
+    assertEquals("LA Geared", testBrand.getName());
+  }
+
+  @Test
+  public void all_listEmptyAtFirst_0() {
+    assertEquals(Brand.listSize(), 0);
+  }
+
+  @Test
+  public void all_listContainsAllBrands_1() {
+    Brand testBrand = new Brand("Ankleys", 2, 1);
+    testBrand.save();
+    assertEquals(Brand.listSize(), 1);
+  }
+
+  @Test
+  public void equals_returnsTrueIfDescribedTheSame() {
+    Brand testBrand = new Brand("Trucks", 4, 2);
+    Brand nextBrand = new Brand("Trucks", 4, 2);
+    assertTrue(testBrand.equals(nextBrand));
+  }
+
+  @Test
+  public void save_savesObjectIntoDatabase() {
+    Brand testBrand = new Brand("Ankleys", 2, 1);
+    testBrand.save();
+    Brand savedBrand = Brand.firstDBEntry();
+    assertTrue(savedBrand.equals(testBrand));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Brand myBrand = new Brand("Ankleys", 2, 1);
+    myBrand.save();
+    Brand savedBrand = Brand.firstDBEntry();
+    assertEquals(myBrand.getId(), savedBrand.getId());
+  }
+
+  @Test
+  public void find_findsBrandInDatabase_true() {
+    Brand myBrand = new Brand("Trucks", 4, 2);
+    myBrand.save();
+    Brand savedBrand = Brand.find(myBrand.getId());
+    assertTrue(myBrand.equals(savedBrand));
+  }
+
+  @Test
+  public void delete_removesObjectFromDatabase() {
+    Brand testBrand = new Brand("Ankleys", 2, 1);
+    testBrand.save();
+    testBrand.delete();
+    assertEquals(Brand.listSize(), 0);
   }
 
 }
