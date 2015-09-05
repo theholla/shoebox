2672af3ae447703235816761dc97bee11b267313
Diana Holland <holla@bikefarm.org>
2015-09-05 15:29:22 -0700

    Add Index, Brand, and Store templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,5 +17,24 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    /* Index --> List of Brands */
+    get("/brands", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      List<Brand> brands = Brand.all();
+      model.put("brands", brands);
+      // model.put("allStores", Store.all());
+      model.put("template", "templates/brands.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    /* Index --> List of Stores */
+    get("/stores", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      List<Store> stores = Store.all();
+      model.put("stores", stores);
+      // model.put("allBrands", Brand.all());
+      model.put("template", "/templates/stores.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
