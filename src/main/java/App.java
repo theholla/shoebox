7ac528576754e0203873b9103fc871555bae528b
theholla <holla@bikefarm.org>
2015-09-04 19:00:19 -0700

    We have contact with web

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    /* Index */
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
 }
