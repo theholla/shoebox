1b39997a44bcc70fbbdfcc6f68bec4fec5187b62
the holla <holla@bikefarm.org>
2015-09-06 12:58:44 -0700

    Let Heroku know about files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/shoebox/bin/shoebox
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    ProcessBuilder process = new ProcessBuilder();
+    Integer port;
+    if (process.environment().get("PORT") != null) {
+      port = Integer.parseInt(process.environment().get(PORT));
+    } else {
+      port = 4567;
+    }
+    
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
