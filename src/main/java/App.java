d24adcce2b5a8556819698be2934e948f0198fd0
the holla <holla@bikefarm.org>
2015-09-06 13:07:00 -0700

    Heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,12 +10,12 @@ public class App {
     ProcessBuilder process = new ProcessBuilder();
     Integer port;
     if (process.environment().get("PORT") != null) {
-      port = Integer.parseInt(process.environment().get(PORT));
+      port = Integer.parseInt(process.environment().get("PORT"));
     } else {
       port = 4567;
     }
     setPort(port);
-    
+
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
